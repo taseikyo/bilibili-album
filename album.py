5f51c6b94a91fa2c6f5a41ac97242d99ab50d978
Tich <lewis.smith.tian@gmail.com>
2017-11-19 21:04:54 +0800

    去掉标题信息(有些标题中含有逗号)

--- album.py
+++ album.py
@@ -12,9 +12,10 @@ import time
 import csv
 from pprint import pprint
 import requests
+import random
 
 # http://h.bilibili.com/{doc_id}
-header = ['up', 'doc_id', 'view_count', 'like_count', 'title', 'upload_timestamp', 'collect_count']                             
+header = ['up', 'doc_id', 'view_count', 'like_count', 'collect_count', 'upload_timestamp']                             
 Album = namedtuple('Album', header)
 headers = {
     'Host':'api.vc.bilibili.com',
@@ -30,7 +31,7 @@ def run(url):
     global total
     r = requests.get(url, headers = headers, timeout = 6).json()
     if r['code'] == 0:
-        time.sleep(1)     # 延迟
+        time.sleep(0.2 + random.randint(60, 80)/100)     # 延迟
         data = r['data']['item']
         up = r['data']['user']
         album = Album(
@@ -38,27 +39,35 @@ def run(url):
             data['doc_id'],             # id
             data['view_count'],         # 浏览次数
             data['like_count'],         # 点赞
-            data['title'],              # 标题
-            data['upload_timestamp'],   # 上传时间
-            data['collect_count']       # 收藏数
+            data['collect_count'],      # 收藏数
+            data['upload_timestamp']    # 上传时间
         )
         with lock:
             result.append(album)
             total += 1
-            print(total)
     else:
         sleep(0.5)
 
-def save():
-    with open('result.csv', 'w+', encoding='utf-8') as f:
+def save(page):
+    with open('album_{}.csv'.format(page), 'w+', encoding='utf-8') as f:
         f_csv = csv.writer(f)
         f_csv.writerow(header)
         f_csv.writerows(result)
 
-
 if __name__ == '__main__':
-    urls = ['http://api.vc.bilibili.com/link_draw/v1/doc/detail?doc_id={}'.format(i)
-            for i in range(100000000)]
-    with futures.ThreadPoolExecutor(32) as executor:
-        executor.map(run, urls)
-    save()
+    for x in range(0, 41):
+        result = []
+        urls = ['http://api.vc.bilibili.com/link_draw/v1/doc/detail?doc_id={}'.format(i)
+                for i in range(x*25000, (x+1)*25000)]
+        with futures.ThreadPoolExecutor(32) as executor:
+            executor.map(run, urls)
+        print(total)
+        save(x)
+
+
+# if __name__ == '__main__':
+#     urls = ['http://api.vc.bilibili.com/link_draw/v1/doc/detail?doc_id={}'.format(i)
+#             for i in range(100000, 150000)]
+#     with futures.ThreadPoolExecutor(32) as executor:
+#         executor.map(run, urls)
+#     save()
